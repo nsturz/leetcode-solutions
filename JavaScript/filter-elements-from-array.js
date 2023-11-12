4a5f003447d0e2ad6e3975672efb77e5f58e6026
Nicolas Sturz <nicksturz92@gmail.com>
2023-11-12 16:27:10 +0000

    completed filter-elements-from-array challenge

--- JavaScript/filter-elements-from-array.js
+++ JavaScript/filter-elements-from-array.js
@@ -39,3 +39,12 @@
 
 // ************************************
 // My solution:
+
+var filter = function(arr, fn) {
+    var filteredArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if (fn(arr[i],i)){
+            filteredArr.push(arr[i]);
+        }
+    } return filteredArr
+};
