58b4b35639931898345ad406a84e632a39621f9c
Nicolas Sturz <nicksturz92@gmail.com>
2023-11-28 14:58:22 +0000

    completed memoize challenge

--- JavaScript/memoize.js
+++ JavaScript/memoize.js
@@ -1,3 +1,76 @@
 // Source: https://leetcode.com/problems/memoize/?envType=study-plan-v2&envId=30-days-of-javascript
 // Author : Nick Sturz
 // Date   : 11/24/2023
+
+//+++++++++++++++++++++++++++++++++++++++++++++++
+
+// Given a function fn, return a memoized version of that function.
+
+// A memoized function is a function that will never be called twice with the same inputs. Instead it will return a cached value.
+
+// You can assume there are 3 possible input functions: sum, fib, and factorial.
+
+// sum accepts two integers a and b and returns a + b.
+// fib accepts a single integer n and returns 1 if n <= 1 or fib(n - 1) + fib(n - 2) otherwise.
+// factorial accepts a single integer n and returns 1 if n <= 1 or factorial(n - 1) * n otherwise.
+ 
+
+// Example 1:
+
+// Input:
+// fnName = "sum"
+// actions = ["call","call","getCallCount","call","getCallCount"]
+// values = [[2,2],[2,2],[],[1,2],[]]
+// Output: [4,4,1,3,2]
+// Explanation:
+// const sum = (a, b) => a + b;
+// const memoizedSum = memoize(sum);
+// memoizedSum(2, 2); // "call" - returns 4. sum() was called as (2, 2) was not seen before.
+// memoizedSum(2, 2); // "call" - returns 4. However sum() was not called because the same inputs were seen before.
+// // "getCallCount" - total call count: 1
+// memoizedSum(1, 2); // "call" - returns 3. sum() was called as (1, 2) was not seen before.
+// // "getCallCount" - total call count: 2
+// Example 2:
+
+// Input:
+// fnName = "factorial"
+// actions = ["call","call","call","getCallCount","call","getCallCount"]
+// values = [[2],[3],[2],[],[3],[]]
+// Output: [2,6,2,2,6,2]
+// Explanation:
+// const factorial = (n) => (n <= 1) ? 1 : (n * factorial(n - 1));
+// const memoFactorial = memoize(factorial);
+// memoFactorial(2); // "call" - returns 2.
+// memoFactorial(3); // "call" - returns 6.
+// memoFactorial(2); // "call" - returns 2. However factorial was not called because 2 was seen before.
+// // "getCallCount" - total call count: 2
+// memoFactorial(3); // "call" - returns 6. However factorial was not called because 3 was seen before.
+// // "getCallCount" - total call count: 2
+// Example 3:
+
+// Input:
+// fnName = "fib"
+// actions = ["call","getCallCount"]
+// values = [[5],[]]
+// Output: [8,1]
+// Explanation:
+// fib(5) = 8 // "call"
+// // "getCallCount" - total call count: 1
+
+//+++++++++++++++++++++++++++++++++++++++++++++++
+//My Solution:
+
+function memoize(fn) { // define memoize that takes a funtion as a parameter
+    let cache = {}; // create empty object that will hold "cache" the result of each function call
+
+    return function(...args) { // return function that takes any number of args
+      let key = JSON.stringify(...args) // use JSON.stringify() to convert args into a string and save them to a variable
+      if (key in cache){ // check if the args are currently stored in cache
+          return cache[key];// if they are, return cache at the appropriate key 
+      } const result = fn.apply(this, args); // if not, call the apply method on fn and pass it args of this and args. save them to a variable
+        cache[key] = result; // store the result in cache and save it to a variable
+        return result; // return the result
+
+
+    }
+}
